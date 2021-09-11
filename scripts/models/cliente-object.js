9cb470e2b1d9f30112c5ffc71605ae19167cd1ec
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-09-11 16:04:19 -0300

    Se arma objeto de clientes

---
+++ scripts/models/cliente-object.js
@@ -0,0 +1,8 @@
+export class ClienteObject {
+    constructor(nombre, apellido, dni, telefono){
+        this.nombre = nombre,
+        this.apellido = apellido,
+        this.dni = dni
+        this.telefono = telefono
+    }
+}

15f5260604336108b1ad4a341d02c9e753410751
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-09-11 16:04:31 -0300

    Se arma objeto de actividades

---
+++ scripts/models/actividad-object.js
@@ -0,0 +1,8 @@
+export class ActividadObject{
+    constructor(id, img, nombre, horarios){
+        this.id = id,
+        this.img = img,
+        this.nombre = nombre,
+        this.horarios = horarios
+    }
+}

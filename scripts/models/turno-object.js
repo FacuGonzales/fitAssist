c0a0f767f74f28ba5aac9236b6c9eb825807db65
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-09-11 16:04:03 -0300

    Se arma objeto de turnos

---
+++ scripts/models/turno-object.js
@@ -0,0 +1,10 @@
+export class TurnoObject{
+    constructor(id, nombreCliente, dniCliente, telefonoCliente, actividadSeleccionada, horarioSeleccionado){
+        this.id = id,
+        this.nombreCliente = nombreCliente,
+        this.dniCliente = dniCliente,
+        this.telefonoCliente = telefonoCliente
+        this.actividadSeleccionada = actividadSeleccionada,
+        this.horarioSeleccionado = horarioSeleccionado
+    }
+}
